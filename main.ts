3997deab3e7c6daf48e468e00387afbff31902d2
ColourfulMelon <jordynnitch@gmail.com>
2025-01-20 01:26:36 +0200

    add matchall regex

--- main.ts
+++ main.ts
@@ -4,9 +4,9 @@ import { getRequiredEnvVar } from './utils/getRequiredEnvVar.ts';
 const bot = new Bot(getRequiredEnvVar('TG_BOT_TOKEN'));
 
 const APP_URL = getRequiredEnvVar('APP_URL');
-
+const matchAll = /.*/;
 // Handle messages.
-bot.on("message", (ctx) => ctx.reply(`To interact with our bot, use the mini app available at ${APP_URL} or click the "App" button on the left.`));
+bot.hears(matchAll, (ctx) => ctx.reply(`To interact with our bot, use the mini app available at ${APP_URL} or click the "App" button on the left.`));
 
 // Start the bot.
 bot.start();
