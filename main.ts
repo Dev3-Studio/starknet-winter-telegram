3a2c247c5e8018f89bd4d2b113eda6076f273641
Xirynx <64374129+Xirynx@users.noreply.github.com>
2025-01-20 05:06:10 +0200

    feat: Improve response logic

--- main.ts
+++ main.ts
@@ -1,12 +1,22 @@
-import { Bot } from "grammy";
+import { Bot } from "https://deno.land/x/grammy@v1.34.0/mod.ts";
 import { getRequiredEnvVar } from './utils/getRequiredEnvVar.ts';
+import { InlineKeyboard } from 'npm:grammy@1.34.0';
 
-const bot = new Bot(getRequiredEnvVar('TG_BOT_TOKEN'));
+const bot = new Bot(getRequiredEnvVar('TG_BOT_TOKEN'), {
+    client: {
+        environment: getRequiredEnvVar('TG_ENVIRONMENT') as 'test' | 'prod',
+    }
+});
 
 const APP_URL = getRequiredEnvVar('APP_URL');
 const matchAll = /.*/;
+const keyboard = new InlineKeyboard().game("Start TeleSwap");
+
 // Handle messages.
-bot.hears(matchAll, (ctx) => ctx.reply(`To interact with our bot, use the mini app available at ${APP_URL} or click the "App" button on the left.`));
+bot.hears(matchAll, (ctx) => ctx.replyWithGame("dex", { reply_markup: keyboard }));
+bot.on("callback_query:game_short_name", async (ctx) => {
+    await ctx.answerCallbackQuery({ url: APP_URL });
+});
 
 // Start the bot.
 bot.start();
